d9e82b9b164c4c25c31028787dbb7669f9106c1a
Patcharapong Samantarn (Bes) <patcharapong@isidsea.th>
2025-05-21 16:41:28 +0700

    Fix time

--- controllers/new-controller.js
+++ controllers/new-controller.js
@@ -1,6 +1,7 @@
 const prisma = require("../models");
 const tryCatch = require("../utils/try-catch");
 const createError = require("../utils/create-error");
+const { DateTime } = require("luxon");
 
 module.exports.newTranInfo = tryCatch(async (req, res, next) => {
   // find all users
@@ -119,7 +120,11 @@ module.exports.addNewTran = tryCatch(async (req, res, next) => {
   ) {
     createError(400, "Lack data!");
   }
-  const combinedDateTime = new Date(`${recordDate}T${recordTime}`);
+  const combinedDateTime = DateTime.fromISO(`${recordDate}T${recordTime}`, {
+    zone: "Asia/Bangkok",
+  })
+    .toUTC()
+    .toJSDate();
   await prisma.Tran.create({
     data: {
       userId: Number(req.user.userId),
