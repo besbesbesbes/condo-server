3c10be85c6b09f91fe9c65c84dd2b898fbcd3fe6
BesRoom <info.besroom@gmail.com>
2025-09-22 11:38:22 +0700

    fix email

--- controllers/mail-controller.js
+++ controllers/mail-controller.js
@@ -2,29 +2,44 @@ const prisma = require("../models/index");
 const tryCatch = require("../utils/try-catch");
 const createError = require("../utils/create-error");
 const nodemailer = require("nodemailer");
-const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
-const mailerSend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY });
+const { google } = require("googleapis");
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 module.exports.newTran = tryCatch(async (req, res, next) => {
   const { to, subject, text } = req.body;
+  const accessToken = await oAuth2Client.getAccessToken();
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      type: "OAuth2",
+      user: "smt.bes@gmail.com",
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken: accessToken.token,
     },
   });
-
+  const htmlText = text.replace(/\n/g, "<br>");
   const mailOptions = {
-    from: `"KB Admin" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
+    from: '"<KB Admin>" <smt.bes@gmail.com>',
+    to: to,
+    subject: subject,
+    text: text,
+    html: `<p>${htmlText}</p>`,
   };
 
-  const info = await transporter.sendMail(mailOptions);
-  console.log("Email sent:", info.response);
+  await transporter.sendMail(mailOptions);
 
   res.json({ message: "Email sent successfully" });
 });
