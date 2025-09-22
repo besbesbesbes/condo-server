7db304c3a3a9c7e0c3dee592d088665cfd4bdc32
BesRoom <info.besroom@gmail.com>
2025-09-22 09:07:42 +0700

    fix email

--- controllers/mail-controller.js
+++ controllers/mail-controller.js
@@ -2,19 +2,18 @@ const prisma = require("../models/index");
 const tryCatch = require("../utils/try-catch");
 const createError = require("../utils/create-error");
 const nodemailer = require("nodemailer");
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
+const mailerSend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY });
 
 module.exports.newTran = tryCatch(async (req, res, next) => {
   const { to, subject, text } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465, // or 587
-    secure: true, // true for 465, false for 587
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 10000, // 10s timeout so it won't hang forever
   });
 
   const mailOptions = {
